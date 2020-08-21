9c75d8f047f0a463475f04d2a16ba97c6b86d98c
Kasper <kasperkh.kh@gmail.com>
2020-08-21 16:21:56 +0200

    Added github token input, improved how locally running works

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # README Template
 
-Add GitHub data to your `README.md`, Or any other file.
+Add GitHub data to your `README.md`, or any other file.
 
 A GitHub action that provides template strings that are replaced with their respective values when the action runs.
 
@@ -8,13 +8,11 @@ By default, it takes `TEMPLATE.md` and outputs `README.md`.
 
 ## Inputs
 
-### `template`
-
-**Required** The file to use as template. Default: `"TEMPLATE.md"`
-
-### `output`
-
-**Required** The output file. Default: `"README.md"`
+| name         | type   | default         | description |
+| ------------ | ------ | --------------- | ----------- |
+| github_token | string |                 | GitHub personal access token used to fetch data. Can be passed using `${{ secrets.GITHUB_TOKEN }}`. [Go here](https://github.com/settings/tokens/new?scopes=read:user) to generate one with the `read:user` scope
+| template     | string | `"TEMPLATE.md"` | Template file path
+| readme       | string | `"README.md"`   | Output file path
 
 ## Example workflow
 ```
@@ -44,4 +42,21 @@ jobs:
             git commit -m "Update README"
             git push
           fi
+```
+
+## Dev instructions
+
+### Test offline
+First, to get started:
+1. Install [Node.js](https://nodejs.org/)
+2. Run `npm install`
+3. [Go here](https://github.com/settings/tokens/new?scopes=read:user) to generate a GitHub personal access token with the `read:user` scope.
+4. Create a `.env` file contianing the token:
+```
+INPUT_GITHUB_TOKEN=mytoken
+```
+
+Now you can test the action by running the following command:
+```
+npm run test
 ```
--- index.js
+++ index.js
@@ -4,74 +4,80 @@ const path = require('path')
 const fs = require('fs')
 const process = require('process')
 
-try {
-  let templateInput = process.env.MODE === 'test' ? 'TEST.md' : core.getInput('template')
-  console.log('template:', templateInput)
-  const templatePath = path.resolve(templateInput);
+run()
+async function run() {
+  try {
 
-  let outputInput = process.env.MODE === 'test' ? 'TEST_OUTPUT.md' : core.getInput('output');
-  console.log('output:', outputInput)
-  const outputPath = path.resolve(outputInput);
+    let templateInput = core.getInput('TEMPLATE')
+    console.log('template:', templateInput)
+    const templatePath = path.resolve(templateInput);
 
-  function trimRightChar(string, charToRemove) {
-      // while(string.charAt(0)==charToRemove) {
-      //     string = string.substring(1);
-      // }
+    let outputInput = core.getInput('OUTPUT');
+    console.log('output:', outputInput)
+    const outputPath = path.resolve(outputInput);
 
-      while(string.charAt(string.length-1)==charToRemove) {
-          string = string.substring(0,string.length-1);
-      }
+    const ghToken = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(ghToken);
 
-      return string;
-  }
-
-  function inject(str, name, valueList) {
-    const startStr = '{{ '+name+': }}';
-    const startIndex = str.indexOf(startStr);
-    const endStr = '{{ /'+name+' }}';
-    const endIndex = str.indexOf(endStr);
-    if (startIndex >= 0 && endIndex >= 0) {
-      let beforeLoop = str.substring(0, startIndex);
-      beforeLoop = trimRightChar(beforeLoop, ' ');
-      beforeLoop = trimRightChar(beforeLoop, '\n');
-      let loopContent = str.substring(startIndex+startStr.length, endIndex);
-      loopContent = trimRightChar(loopContent, ' ');
-      loopContent = trimRightChar(loopContent, '\n');
-      let newLoopContent = ''
-      const afterLoop = str.substring(endIndex+endStr.length);
-      // loop through array
-      for (const value of valueList) {
-        // loop through value object
-        let newNewLoopContent = loopContent
-        for (const key in value) {
-          if (!value.hasOwnProperty(key)) return;
-          const valueStr = '{{ '+key+' }}';
-          newNewLoopContent = newNewLoopContent.split(valueStr).join(value[key]);
+    function trimRightChar(string, charToRemove) {
+        // while(string.charAt(0)==charToRemove) {
+        //     string = string.substring(1);
+        // }
+        while(string.charAt(string.length-1)==charToRemove) {
+            string = string.substring(0,string.length-1);
         }
-        newLoopContent += newNewLoopContent
-      }
-      return beforeLoop + newLoopContent + inject(afterLoop, name, valueList);
-    } else {
-      return str;
+        return string;
     }
+
+    function inject(str, name, valueList) {
+      const startStr = '{{ '+name+': }}';
+      const startIndex = str.indexOf(startStr);
+      const endStr = '{{ /'+name+' }}';
+      const endIndex = str.indexOf(endStr);
+      if (startIndex >= 0 && endIndex >= 0) {
+        let beforeLoop = str.substring(0, startIndex);
+        beforeLoop = trimRightChar(beforeLoop, ' ');
+        beforeLoop = trimRightChar(beforeLoop, '\n');
+        let loopContent = str.substring(startIndex+startStr.length, endIndex);
+        loopContent = trimRightChar(loopContent, ' ');
+        loopContent = trimRightChar(loopContent, '\n');
+        let newLoopContent = ''
+        const afterLoop = str.substring(endIndex+endStr.length);
+        // loop through array
+        for (const value of valueList) {
+          // loop through value object
+          let newNewLoopContent = loopContent
+          for (const key in value) {
+            if (!value.hasOwnProperty(key)) return;
+            const valueStr = '{{ '+key+' }}';
+            newNewLoopContent = newNewLoopContent.split(valueStr).join(value[key]);
+          }
+          newLoopContent += newNewLoopContent
+        }
+        return beforeLoop + newLoopContent + inject(afterLoop, name, valueList);
+      } else {
+        return str;
+      }
+    }
+
+    const templateFile = fs.readFileSync(templatePath).toString();
+    const result = inject(templateFile, 'REPOSITORIES', [
+      {
+        NAME: 'cpc',
+        STARS: 2,
+        DESCRIPTION: 'calculaton + conversion',
+      },
+      {
+        NAME: 'vidl',
+        STARS: 5,
+        DESCRIPTION: 'youtube video downloader',
+      },
+    ]);
+
+    fs.writeFileSync(outputPath, result)
+
+  } catch (error) {
+    console.log(error)
+    core.setFailed(error.message);
   }
-
-  const templateFile = fs.readFileSync(templatePath).toString();
-  const result = inject(templateFile, 'REPOSITORIES', [
-    {
-      NAME: 'cpc',
-      STARS: 2,
-      DESCRIPTION: 'calculaton + conversion',
-    },
-    {
-      NAME: 'vidl',
-      STARS: 5,
-      DESCRIPTION: 'youtube video downloader',
-    },
-  ]);
-
-  fs.writeFileSync(outputPath, result)
-} catch (error) {
-  console.log(error)
-  core.setFailed(error.message);
 }
